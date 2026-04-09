c77497be995d0206f174b556ed2b3d4b78da098e
chaussin-a <2220584499@qq.com>
2026-04-09 17:52:44 +0800

    test

--- main/app_main.c
+++ main/app_main.c
@@ -17,6 +17,6 @@ void app_main(void)
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_at_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-
+    // test
     esp_at_init();
 }
